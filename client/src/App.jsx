b44531ed9aeb0fdf4e964122ef7eeaccafebfce6
Naman Rajput <namanrajput801@gmail.com>
2024-12-20 23:32:30 +0530

    ui:make her section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Login from "./pages/Login";
 import HeroSection from "./pages/student/HeroSection";
 import MainLayout from "./layout/MainLayout";
+import Courses from "./pages/student/Courses";
 const appRouter = createBrowserRouter([{
   path:"/",
   element:<MainLayout/>,
@@ -12,6 +13,7 @@ const appRouter = createBrowserRouter([{
           element:(
               <>
               <HeroSection/>
+              <Courses/>
               </>
           )
          
--- client/src/pages/student/HeroSection.jsx
+++ client/src/pages/student/HeroSection.jsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
         <h1 className='text-white text-4xl font-bold mb-4'>Find the Best courcses for you</h1>
         <p className='text-gray-200 dark:text-gray-400 mb-8'>Discover, Learn and Unskill yourself</p>
         <form action="" className=' flex items-center bg-white dark:bg-gray-800 rounded-full shadow-lg overflow-hidden max-w-xl mx-auto mb-6'>
-            <Input type="text" className="flex-grow border-none focus-visible:ring-0 px-6 py-3 text-gray-900 dark:text-gray-100 placeholder:bg-gray-400 dark:placeholder:-gray-500"/>
+            <Input type="text" className="flex-grow border-none focus-visible:ring-0 px-6 py-3 text-gray-900 dark:text-gray-100  dark:placeholder:-gray-500" placeholder="Search Courses"/>
             <Button className="bg-blue-600 dark:bg-gray-700 text-white px-6 py-3 rounded-r-full hover:bg-blue-700 ">Search</Button>
         </form>
             <Button className="bg-white dark:bg-gray-800 text-blue-600 rounded-full hover:bg-gray-200">Explore Cources</Button>
